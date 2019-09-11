4cf6b2017f5ad541581691748670126c1cdb07f7
wustboy001 <1139212354@qq.com>
2019-09-11 16:46:10 +0800

    第二次提交

--- src/main/java/testdemo.java
+++ src/main/java/testdemo.java
@@ -6,4 +6,7 @@ public class testdemo {
     public static void main(String[] args) {
         System.out.println("这是一个测试demo");
     }
+    private void f(){
+        System.out.println("hello!!!");
+    }
 }
